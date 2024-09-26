de26e4ac498da641aa8b438b1d6acaf11c5cea3f
Ali Nazem <162388335+DOH-AXN0303@users.noreply.github.com>
2024-09-26 13:36:22 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -179,8 +179,8 @@ if prompt:
                     response.resolve()
                 else:
                     response = st.session_state.chat.send_message(prmt[0])
-            with st.chat_message('ai'):
-                st.write(response.text) 
+                with st.chat_message('ai'):
+                    st.write(response.text) 
                 
             except Exception as e:
                 with st.chat_message('ai'):
